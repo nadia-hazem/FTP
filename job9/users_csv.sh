b4830532c55c8967a289f09a06f097fb9c317795
Pictelle <114389825+nadia-hazem@users.noreply.github.com>
2022-10-07 00:48:26 +0200

    Update users_csv.sh

--- job9/users_csv.sh
+++ job9/users_csv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#cat /home/Documents/laplateforme/FTP/job9/users.csv | while read varligne
+#cat ~/Documents/laplateforme/FTP/job9/users.csv | while read varligne
 #do
  #   password=`echo $varligne |cut -d ',' -f4`
  #   username=`echo $varligne |cut -d ',' -f2`
